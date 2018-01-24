9b4d8e554fdfeafc23bf189817fc526f7c9389e6
Christy Lu <hyl9@uw.edu>
2018-01-23 16:41:25 -0800

    lhs & rhs

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -54,5 +54,18 @@ class Calculator {
         return lhs + rhs
     }
     
+    public func subtract(lhs : Int, rhs : Int) -> Int {
+        return lhs - rhs
+    }
+    
+    public func multiply(lhs : Int, rhs : Int) -> Int {
+        return lhs * rhs
+    }
+    
+    public func divide(lhs : Int, rhs : Int) -> Int {
+        return lhs / rhs
+    }
+    
+    
     
 }
--- ComplexCalc/ComplexCalc/main.swift
+++ ComplexCalc/ComplexCalc/main.swift
@@ -17,11 +17,11 @@ let calc = Calculator()  // Don't change this declaration name; it's used in all
 
 // ====> Do not modify code in this section <====
 assert(calc.add(lhs: 2, rhs: 2) == 4)
-/*assert(calc.subtract(lhs: 2, rhs: 2) == 0)
+assert(calc.subtract(lhs: 2, rhs: 2) == 0)
 assert(calc.multiply(lhs: 2, rhs: 2) == 4)
 assert(calc.divide(lhs: 2, rhs: 2) == 1)
 
-assert(calc.mathOp(lhs: 5, rhs: 5, op: { ($0 + $1) + ($0 * $1) }) == 35)
+/*assert(calc.mathOp(lhs: 5, rhs: 5, op: { ($0 + $1) + ($0 * $1) }) == 35)
 assert(calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20)*/
 
 assert(calc.add([1, 2, 3, 4, 5]) == 15)
